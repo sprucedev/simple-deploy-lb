9ff173155adfa9c5d803f70e8a21cf45bab4ce5c
Ricky Cook <mail@thatpanda.com>
2015-12-21 23:56:44 +1100

    Update host re

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ INIT="/etc/init.d/haproxy"
 CONF_PATH="/etc/haproxy/haproxy.cfg"
 CONF_D="$CONF_PATH.d"
 
-export HOST_RE='simple-deploy'
+export HOST_RE='sdeploy-'
 
 function regenerate {
   for filename in $(ls "$CONF_D"); do
